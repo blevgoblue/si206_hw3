8b74fb31fb38184af7b3d7eefbd2f85fbf279e10
Brandon Levine <levinebr@umich.edu>
2016-11-11 13:59:48 -0500

    Prompt for Parts of Speech.

--- madlibhw3.py
+++ madlibhw3.py
@@ -35,9 +35,16 @@ original_text_list = map(lambda word: spaced(word), tokens)
 print("".join(original_text_list))
 
 print("\n\n----------Choose Words:----------\n\n")
-# 5 Parts of speech: Verb, Adjective, Noun, Plural Noun, Adverb
+# 5 Parts of speech: Verb, Adjective, Noun, Plural Noun, Adverb.
+parts_of_speech = {"VB" : [0.10, "a Verb", ""], "JJ" : [0.10, "an Adjective", ""], "NN" : [0.15, "a Noun", ""], "NNS" : [0.15, "a Plural Noun", ""], "AD" : [0.10, "an Adverb", ""]}
 
 # Ask for word of appropriate type
+# Loop over all tags and prompt for a word
+for tag in parts_of_speech:
+    # Store word in dict
+    parts_of_speech[tag][2] = input("Please enter " + parts_of_speech[tag][1] + ": ")
+
+print (parts_of_speech)
 
 # Follow logic from example to change words
 
